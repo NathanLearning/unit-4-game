1f9e71e63f71377cf74b575b7808453ad23911c4
Nathan Visti <nathanvisti@gmail.com>
2019-06-06 22:55:52 -0400

    Created random number variable + egg values

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    var randomNum = Math.floor(Math.random()*101+19)
+    var blue = Math.floor(Math.random()*11+1)
+    var pink = Math.floor(Math.random()*11+1)
+    var green = Math.floor(Math.random()*11+1)
+    var purple = Math.floor(Math.random()*11+1)
+})
